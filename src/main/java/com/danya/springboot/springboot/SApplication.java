d3cb4a04911b77eb327bfcdeefdddc0a81bf2022
danya <numnums021@gmail.com>
2022-02-17 20:00:46 +0300

    Initial commit

---
+++ src/main/java/com/danya/springboot/springboot/SApplication.java
@@ -0,0 +1,13 @@
+package com.danya.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danya/springboot/springboot/SApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danya.springboot.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
